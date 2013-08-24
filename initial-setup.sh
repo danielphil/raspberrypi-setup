a414a30cd9b1bb9575f89f3c4140c93f2f413d3e
Daniel Phillips <dphildev@gmail.com>
2013-08-24 12:00:44 +0000

    Now installs node.js and adds it to the path

--- initial-setup.sh
+++ initial-setup.sh
@@ -6,9 +6,26 @@ if [[ $UID != 0 ]]; then
     echo "Please start the script as root or sudo!"
     exit 1
 fi
+
+# update the packages that we already have
 apt-get update
 apt-get upgrade --yes
+
+# install avahi for easier SSH access
 apt-get --yes install avahi-daemon
 insserv avahi-daemon
 cp avahi-config /etc/avahi/services/multiple.service
 /etc/init.d/avahi-daemon restart
+
+# install nodejs
+mkdir /tmp/node
+pushd /tmp/node
+NODENAME=node-v0.10.17-linux-arm-pi
+wget --output-document=/tmp/node/$NODENAME.tar.gz http://nodejs.org/dist/v0.10.17/$NODENAME.tar.gz
+tar xvzf /tmp/node/$NODENAME.tar.gz
+mv -v /tmp/node/$NODENAME/ /usr/local/$NODENAME
+popd
+rm -rfv /tmp/node
+
+#add nodejs to path
+echo "export PATH=\$PATH:/usr/local/$NODENAME/bin" >> /home/pi/.bash_profile
